2019a8e8a29ebe147859b34c127038a955157c4d
noah-berman <noahberm@gmail.com>
2018-10-01 10:31:27 -0400

    config for heroku

--- config/puma.rb
+++ config/puma.rb
@@ -4,8 +4,13 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -13,6 +18,12 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
+
+on_worker_boot do
+  # Valid on Rails 4.1+ using the `config/database.yml` method of setting `pool` size
+  ActiveRecord::Base.establish_connection
+end
+
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the number of `workers` to boot in clustered mode.
